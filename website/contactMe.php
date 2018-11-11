74e55a60a84568b5c69116b757934d2004878694
Christopher Littlewood <christopher.littlewood@connect.qut.edu.au>
2018-11-12 08:54:11 +1000

    Contact Me Validation v1
    Fixed the server side validation for the contact me page.

--- website/contactMe.php
+++ website/contactMe.php
@@ -82,7 +82,9 @@
 
     //Validate the email:
     function validateEmail($Email){
-        if(filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+
+        $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+        if(preg_match($pattern, $Email)) {
             return true;
         }
         return false;
@@ -90,7 +92,7 @@
 
     //Validate the user's name:
     function validateName($Name){
-        $pattern = '/^[\p{L}\p{P}\p{Zs}]+$/';
+        $pattern = '/^[A-Za-z ]+$/';
         if (preg_match($pattern, $Name)) {
             return true;
         }else{
@@ -100,7 +102,7 @@
 
     //Validate the catagory:
     function validateCatagory($Catagory){
-        if($Catagory == "--Select Catagory--"){
+        if(strcmp($Catagory, '--Select Category--') == 0){
             return false;
         }else{
             return true;
